469f441cc164b804b48e81a5093e0a6454265686
Jakub Gause <gause.jakub@gmail.com>
2022-07-01 11:22:59 +0200

    Init rtc

--- automatic-irrigation-for-chameleon.ino
+++ automatic-irrigation-for-chameleon.ino
@@ -1,9 +1,43 @@
-#define LED 13  // The pin the LED is connected to
+#include <DS3231.h>
+
+int Relay = 4;
+
+DS3231  rtc(SDA, SCL);
+Time t;
+
+const int OnHour = 11; //SET TIME TO ON RELAY (24 HOUR FORMAT)
+const int OnMin = 18;
+const int OffHour = 11; //SET TIME TO OFF RELAY
+const int OffMin = 19;
 
 void setup() {
-  pinMode(LED, OUTPUT); // Declare the LED as an output
+  Serial.begin(115200);
+  rtc.begin();
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, LOW);
 }
 
 void loop() {
-  digitalWrite(LED, HIGH); // Turn the LED on
+  t = rtc.getTime();
+ // Send Day-of-Week
+  Serial.print(rtc.getDOWStr());
+  Serial.print(" ");
+  
+  // Send date
+  Serial.print(rtc.getDateStr());
+  Serial.print(" -- ");
+
+  // Send time
+  Serial.println(rtc.getTimeStr());
+  delay (1000);
+  
+  if(t.hour == OnHour && t.min == OnMin){
+    digitalWrite(Relay,HIGH);
+    Serial.println("LIGHT ON");
+    }
+    
+    else if(t.hour == OffHour && t.min == OffMin){
+      digitalWrite(Relay,LOW);
+      Serial.println("LIGHT OFF");
+    }
 }
